28ac97900c25f0b25e33bd917f0f32e42ee4a49d
Fabricio <medinajose3195@gmail.com>
2022-08-01 19:30:46 +0200

    Aplica validaciones al formulario

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,30 @@
-import { useContext, useRef } from "react";
+import { useState, useContext } from "react";
 import { Form, Button } from "react-bootstrap";
 import { NewsContext } from "../../context/newsContext";
 
 const Search = () => {
-  const inputRef = useRef();
+  const [form, setForm] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
   const { getTerm } = useContext(NewsContext);
 
+  const handleChange = (e) => {
+    if (e.target.value.length >= 3) setButtonDisabled(false);
+    else setButtonDisabled(true);
+
+    setForm(e.target.value);
+  };
+
+  const clearForm = () => {
+    setForm("");
+    setButtonDisabled(true);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    getTerm(inputRef.current.value);
+
+    getTerm(form);
+    clearForm();
   };
 
   return (
@@ -19,12 +34,15 @@ const Search = () => {
           type="text"
           placeholder="Buscar noticia..."
           className="py-2 mb-3 border-2 border-secondary shadow-none"
-          ref={inputRef}
+          name="form"
+          value={form}
+          onChange={handleChange}
         />
         <Button
           type="submit"
           variant="dark"
           className="btn-sm px-3 py-2 text-uppercase shadow-none"
+          disabled={buttonDisabled}
         >
           Buscar
         </Button>

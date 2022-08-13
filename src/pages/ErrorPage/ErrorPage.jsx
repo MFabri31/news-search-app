67e05fc320cef9315d80d2c9d798e80b9e03f3be
MFabri31 <medinajose3195@gmail.com>
2022-08-13 19:25:34 -0300

    Añade la pagina 404

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,19 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <>Página 404</>;
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md={8} className="text-center mx-auto py-5">
+            <h1 className="display-5">Página no encontrada!</h1>
+            <NavLink to="/">Volver</NavLink>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default ErrorPage;

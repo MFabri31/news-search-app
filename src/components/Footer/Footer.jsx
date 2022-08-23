ccbf858b7cb62785b1f38d9e272b8922f169196c
MFabri31 <medinajose3195@gmail.com>
2022-08-23 20:01:18 -0300

    Corrige estilos del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bg-light py-3 text-center position-absolute bottom-0 w-100">
-        <span className="text-muted">Copyright 2022</span>
+      <footer className="bg-light py-3 text-muted text-center position-absolute bottom-0 w-100 ">
+        <span>Developed by @MFabri31</span>
       </footer>
     </>
   );

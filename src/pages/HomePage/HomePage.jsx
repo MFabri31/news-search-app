5624a7470ce1bd0f4379af71e380b17bbcca3a6c
MFabri31 <medinajose3195@gmail.com>
2025-02-07 19:11:04 -0300

    Modifica el tamaño del alert + añade el hero

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,16 +4,17 @@ import NewsList from "../../components/NewsList/NewsList";
 import Loading from "../../components/Loading";
 import MyPagination from "../../components/Pagination";
 import { useNewsContext } from "../../hooks/useNewsContext";
+import Hero from "../../components/Hero/Hero";
 
 const HomePage = () => {
   const { news, loading } = useNewsContext();
 
   return (
     <>
-      <Row className="justify-content-center py-5">
-        <Col md={5}>
+      <Row className="justify-content-center mb-5">
+        <Hero>
           <Search />
-        </Col>
+        </Hero>
       </Row>
       <main>
         <Row className="justify-content-center">
@@ -21,7 +22,7 @@ const HomePage = () => {
           <NewsList />
           {news?.length > 0 && <MyPagination />}
           {news?.length === 0 && (
-            <Alert variant="warning" className="w-50 text-center">
+            <Alert variant="warning" className="w-50 text-center fs-2">
               Sin resultados!
             </Alert>
           )}

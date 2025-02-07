bb81637830f22b3baa19ddc965f8370f34006982
MFabri31 <medinajose3195@gmail.com>
2025-02-07 19:07:20 -0300

    Agrega contenido nuevo al footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
+// import "./Footer.css";
+
 const Footer = () => {
   return (
     <>
-      <footer className="bg-light py-3 text-muted text-center position-absolute bottom-0 w-100 ">
-        <span>Developed by @MFabri31</span>
+      <footer className="border-top text-muted text-center position-absolute bottom-0 w-100 py-3 footer">
+        <div className="d-flex gap-3 justify-content-center py-2">
+          <a
+            href="https://www.linkedin.com/in/medina-fabricio-aa81741b5"
+            target="blank"
+          >
+            {" "}
+            <i class="bi bi-linkedin fs-2 text-dark-emphasis"></i>
+          </a>
+          <a href="https://github.com/MFabri31" target="blank">
+            {" "}
+            <i class="bi bi-github fs-2 text-dark-emphasis"></i>
+          </a>
+        </div>
+        <p className="lead fst-italic">
+          Desarrollado con <span className="fst-normal"> ❤️ </span>por
+          <a href="" className="fw-semibold text-decoration-none text-dark">
+            {" "}
+            @MFabri31
+          </a>
+        </p>
       </footer>
     </>
   );
